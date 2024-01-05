21dc2a60035b71d682cd46384b65489978a8cf82
rasmusbothomsen <rasmus0408@gmail.com>
2024-01-05 13:09:34 +0100

    Quarkus go again

--- client-code-updated/src/main/java/com/example/MyBankService.java
+++ client-code-updated/src/main/java/com/example/MyBankService.java
@@ -8,6 +8,7 @@ import dtu.ws.fastmoney.*;
 import dtu.ws.fastmoney.Account;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class MyBankService {
 
@@ -22,7 +23,15 @@ public class MyBankService {
         user.setLastName(lastname);
 
         try {
+            List<AccountInfo> accountInfoList = bank.getAccounts();
+            for (AccountInfo accountInfo : accountInfoList){
+                if(accountInfo.getUser().getCprNumber() == user.getCprNumber()){
+                    return accountInfo.getAccountId();
+                }
+            }
             return bank.createAccountWithBalance(user,new BigDecimal(int1));
+
+
         } catch (BankServiceException_Exception e) {
             return "NO ACCOUNT CREATED";
         }
